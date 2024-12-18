a09ff599360c064bb0ef4ee77cef6254b277a68a
krishnavil <vilaspuremadhukar@gmail.com>
2024-12-18 08:03:41 +0530

    one changes

---
+++ Demo/src/login.java
@@ -0,0 +1,16 @@
+
+public class login {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Logged in page");
+
+	}
+	
+	public void met()
+
+	{
+		System.out.println("Done by one");
+	}
+
+}
